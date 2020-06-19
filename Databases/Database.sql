52e48171b2de1d9b64da7ed4841d6407fc2bff0e
Renzo <renzitofabri@hotmail.com>
2020-06-18 22:38:58 -0500

    Se termino la creacion y relacion de tablas

--- Databases/Database.sql
+++ Databases/Database.sql
@@ -1,5 +1,6 @@
 create database Proyect_sierra_sun;
 use Proyect_sierra_sun;
+
 DROP TABLES if exists Articulos;
 create table Articulos(
 Codigo_A int(6) auto_increment,
@@ -42,8 +43,34 @@ unique key id (id)
 
 alter table Users add constraint fk_UsersAcces foreign key (Acces_id) references Acces (id) on delete cascade on update cascade;
 
+
+
 drop table if exists comunidades;
-create table Comunidades;
+create table Comunidades(
+id_Comunidad int(10) not null auto_increment,
+Nombre varchar(50) not null,
+Ubicacion varchar(50) not null,
+Num_Actividades int(10) default null,
+id_Activid int(10) default null,
+id_Presupuesto int(10) default null,
+primary key  (id_Comunidad),
+unique key id_Comunidad (id_Comunidad)
+);
+
+alter table Comunidades add constraint fk_ComuActivi foreign key (id_Activid) references Actividades (id) on delete restrict on update cascade;
 
 drop table if exists Presupuesto;
-create table Presupuesto;
+create table Presupuesto(
+id_Presu int(10) not null auto_increment,
+Cantidad int(10) not null,
+Area_remitente varchar(50) not null,
+Id_Comunidad int(10) default null,
+primary key  (id_Presu),
+unique key id_Presu (id_Presu));
+
+alter table Comunidades add constraint fk_ComuPresu foreign key (id_Presupuesto) references Presupuesto (id_Presu) on delete restrict on update cascade;
+
+
+
+
+
